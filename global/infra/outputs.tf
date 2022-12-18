ef3277e9b3e4b33e195ef5ede2ea34a0be6fd0d1
Brett Curtis <brett@osinfra.io>
2022-12-18 08:02:01 -0500

    Refactor for_each expressions (#32)

--- global/infra/outputs.tf
+++ global/infra/outputs.tf
@@ -14,13 +14,13 @@ output "project_id" {
 output "github_actions_service_account_emails" {
   description = "The GitHub Actions service account emails"
   value = {
-    for folders_key, service_account in google_service_account.github_actions : folders_key => service_account.email
+    for folder_key, service_account in google_service_account.github_actions : folder_key => service_account.email
   }
 }
 
 output "terraform_state_storage_buckets" {
-  description = "The terraform state bucket names"
+  description = "The Terraform state bucket names"
   value = {
-    for folders_key, storage_bucket in module.terraform_state_storage_bucket : folders_key => storage_bucket.name
+    for folder_key, storage_bucket in module.terraform_state_storage_bucket : folder_key => storage_bucket.name
   }
 }
--- global/infra/tfvars/sb.tfvars
+++ global/infra/tfvars/sb.tfvars
@@ -1,5 +1,5 @@
-cis_2_2_logging_sink_project_id = "shared-logs01-tf3521-sb"
+cis_2_2_logging_sink_project_id = "shared-logs01-tf981b-sb"
 env                             = "sb"
 folder_id                       = "472942506775"
 random_project_id               = true
-workload_identity_pool_name     = "projects/973857465223/locations/global/workloadIdentityPools/github-actions"
+workload_identity_pool_name     = "projects/1089754275893/locations/global/workloadIdentityPools/github-actions"
