492c229d5d29e9d66ea957446699087d272099f1
Brett Curtis <brett@osinfra.io>
2024-12-21 08:27:06 -0500

    Update description in main.tf from "k8s" to "terraform"

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ module "project" {
 
   billing_account                 = var.project_billing_account
   cis_2_2_logging_sink_project_id = var.project_cis_2_2_logging_sink_project_id
-  description                     = "k8s"
+  description                     = "terraform"
   folder_id                       = var.project_folder_id
   labels                          = module.helpers.labels
   monthly_budget_amount           = var.project_monthly_budget_amount
